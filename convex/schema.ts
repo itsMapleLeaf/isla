8f8e884a275eafc0d9f5bbc7df58c1ce7c156176
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-10-19 17:34:24 -0500

    schema theorizing

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,13 +2,19 @@ import { defineSchema, defineTable } from "convex/server"
 import { v } from "convex/values"
 
 export default defineSchema({
-	products: defineTable({
-		title: v.string(),
-		imageId: v.string(),
-		price: v.number(),
+	worlds: defineTable({
+		name: v.string(),
 	}),
-	todos: defineTable({
-		text: v.string(),
-		completed: v.boolean(),
+
+	tasks: defineTable({
+		name: v.string(),
+		worldId: v.id("worlds"),
+	}),
+
+	items: defineTable({
+		name: v.string(),
+		worldId: v.id("worlds"),
+		taskId: v.id("tasks"),
+		collected: v.boolean(),
 	}),
 })
